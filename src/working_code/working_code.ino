a7111051ea4ee26259c882f787c97f711b66f81c
vibqetowi <abutcher.usa@protonmail.com>
2023-11-09 20:24:26 -0500

    push

--- src/working_code/working_code.ino
+++ src/working_code/working_code.ino
@@ -10,112 +10,111 @@
 // define the robot class
 class Robot {
 
-public:
-  // Define the pin numbers for the IR sensors
-  const int  irAnalogSensorLeft;
-  const int irAnalogSensorRight;
+  public:
+    // Define the pin numbers for the IR sensors
+    const int  irAnalogSensorLeft;
+    const int irAnalogSensorRight;
 
-  // Define the pin numbers for the ultrasonic sensor
-  // const int 
-  
-  const int speed; // Define the motor speed
-  
-  // Define the motor pin mappings
-  // for motor, pins with "~" must be used
-  const int motorLeftPinForward;
-  const int motorLeftPinBack;
-  const int motorRightPinForward;
-  const int motorRightPinBack;
+    // Define the pin numbers for the ultrasonic sensor
+    // const int 
+    
+    const int speed; // Define the motor speed
+    
+    // Define the motor pin mappings
+    // for motor, pins with "~" must be used
+    const int motorLeftPinForward;
+    const int motorLeftPinBack;
+    const int motorRightPinForward;
+    const int motorRightPinBack;
 
-  // you may find the logic varies per challenge, therefore 
-  // this variable will tell the robot which challenge you are running
-  const int challengeNumber;
+    // you may find the logic varies per challenge, therefore 
+    // this variable will tell the robot which challenge you are running
+    const int challengeNumber;
 
-  // Constructor
-  // here you enter the pin names for each of the functions, order matters!
+    // Constructor
+    // here you enter the pin names for each of the functions, order matters!
   Robot(int irLeft, int irRight, int motorLeftforward,
-   int motorLeftback, int motorRightforward, int motorRightback, 
-   int speed, int challenge):
-        irAnalogSensorLeft(irLeft),
-        irAnalogSensorRight(irRight),
-        motorLeftPinForward(mLf),
-        motorLeftPinBack(mLb),
-        motorRightPinForward(mRf),
-        motorRightPinBack(mRb),
-        speed(speed),
-        challengeNumber(challenge)
-  {
-  }
+  int motorLeftback, int motorRightforward, int motorRightback, 
+  int speed, int challenge):
+      irAnalogSensorLeft(irLeft),
+      irAnalogSensorRight(irRight),
+      motorLeftPinForward(motorLeftforward),
+      motorLeftPinBack(motorLeftback),
+      motorRightPinForward(motorRightforward),
+      motorRightPinBack(motorRightback),
+      speed(speed),
+      challengeNumber(challenge)
+    {}
 
 
-  void forward() {
-    analogWrite(motorLeftPinForward, speed);
-    analogWrite(motorLeftPinBack, 0);
-    analogWrite(motorRightPinForward, speed);
-    analogWrite(motorRightPinBack, 0);
+    void forward() {
+      analogWrite(motorLeftPinForward, speed);
+      analogWrite(motorLeftPinBack, 0);
+      analogWrite(motorRightPinForward, speed);
+      analogWrite(motorRightPinBack, 0);
 
-    // Serial.print("forward\n");
-  }
+      Serial.print("forward\n");
+    }
 
-  void reverse() {
-    analogWrite(motorLeftPinForward, 0);
-    analogWrite(motorLeftPinBack, speed);
-    analogWrite(motorRightPinForward, 0);
-    analogWrite(motorRightPinBack, speed);
-    // Serial.print("backwards\n");
-  }
+    void reverse() {
+      analogWrite(motorLeftPinForward, 0);
+      analogWrite(motorLeftPinBack, speed);
+      analogWrite(motorRightPinForward, 0);
+      analogWrite(motorRightPinBack, speed);
+      Serial.print("backwards\n");
+    }
 
-  void turn_right() {
-    analogWrite(motorLeftPinForward, speed);
-    analogWrite(motorLeftPinBack, 0);
-    analogWrite(motorRightPinForward, 0);
-    analogWrite(motorRightPinBack, 0);
-    // Serial.print("right\n");
-  }
+    void turn_right() {
+      analogWrite(motorLeftPinForward, speed);
+      analogWrite(motorLeftPinBack, 0);
+      analogWrite(motorRightPinForward, 0);
+      analogWrite(motorRightPinBack, 0);
+      Serial.print("right\n");
+    }
 
-  void turn_left() {
-    analogWrite(motorLeftPinForward, 0);
-    analogWrite(motorLeftPinBack, 0);
-    analogWrite(motorRightPinForward, speed);
-    analogWrite(motorRightPinBack, 0);
-    // Serial.print("left\n");
+    void turn_left() {
+      analogWrite(motorLeftPinForward, 0);
+      analogWrite(motorLeftPinBack, 0);
+      analogWrite(motorRightPinForward, speed);
+      analogWrite(motorRightPinBack, 0);
+      Serial.print("left\n");
 
-  }
+    }
 
-  void stop() {
-    analogWrite(motorLeftPinForward, 0);
-    analogWrite(motorLeftPinBack, 0);
-    analogWrite(motorRightPinForward, 0);
-    analogWrite(motorRightPinBack, 0);
-    // Serial.print("stop\n");
-  }
+    void stop() {
+      analogWrite(motorLeftPinForward, 0);
+      analogWrite(motorLeftPinBack, 0);
+      analogWrite(motorRightPinForward, 0);
+      analogWrite(motorRightPinBack, 0);
+      Serial.print("stop\n");
+    }
 
-// this function returns the values read by the IR sensor and may be useful to you
-  int getIntensity(int irAnalogPin) {
-    int intensity = analogRead(irAnalogPin);
-    Serial.print(intensity);
-    return intensity;
-  }
+  // this function returns the values read by the IR sensor and may be useful to you
+    int getIntensity(int irAnalogPin) {
+      int intensity = analogRead(irAnalogPin);
+      Serial.print(intensity);
+      return intensity;
+    }
 
-};
+  };
 
 
-// what we did here is create a robot object that will be used throughout the program
-// you can experiment with speed, it just changes the amount of current used
-Robot robot(A0, A1, 5,6,9,10, 1000, 1);  
+  // what we did here is create a robot object that will be used throughout the program
+  // you can experiment with speed, it just changes the amount of current used
+  Robot robot(A0, A1, 5,6,9,10, 1000, 1);  
 
-void setup() {
-  Serial.begin(9600);
+  void setup() {
+    Serial.begin(9600);
 
-  // Set the IR sensor pins as input
-  pinMode(robot.irAnalogSensorLeft, INPUT);
-  pinMode(robot.irAnalogSensorRight, INPUT);
+    // Set the IR sensor pins as input
+    pinMode(robot.irAnalogSensorLeft, INPUT);
+    pinMode(robot.irAnalogSensorRight, INPUT);
 
-  // Set the motor control pins as output
-  pinMode(robot.motorLeftPinForward, OUTPUT);
-  pinMode(robot.motorLeftPinBack, OUTPUT);
-  pinMode(robot.motorRightPinForward, OUTPUT);
-  pinMode(robot.motorRightPinBack, OUTPUT);
+    // Set the motor control pins as output
+    pinMode(robot.motorLeftPinForward, OUTPUT);
+    pinMode(robot.motorLeftPinBack, OUTPUT);
+    pinMode(robot.motorRightPinForward, OUTPUT);
+    pinMode(robot.motorRightPinBack, OUTPUT);
 }
 
 IRtreshold = 500
