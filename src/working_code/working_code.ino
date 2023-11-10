36eb9d02889fde65165d82f9c83f52078bb36d92
vibqetowi <abutcher.usa@protonmail.com>
2023-11-09 20:43:07 -0500

    push

--- src/working_code/working_code.ino
+++ src/working_code/working_code.ino
@@ -1,25 +1,22 @@
 #include <Arduino.h>
 // // #include <NewPing.h>
 
-
 // this code follows an object-oriented paradigm where the robot
 // is stored in the code as a class containing attributes
 // (pin numbers and speed) and methods to turn left for instance
 
-
 // define the robot class
 class Robot {
-
   public:
     // Define the pin numbers for the IR sensors
-    const int  irAnalogSensorLeft;
+    const int irAnalogSensorLeft;
     const int irAnalogSensorRight;
 
     // Define the pin numbers for the ultrasonic sensor
     // const int 
-    
+
     const int speed; // Define the motor speed
-    
+
     // Define the motor pin mappings
     // for motor, pins with "~" must be used
     const int motorLeftPinForward;
@@ -33,26 +30,21 @@ class Robot {
 
     // Constructor
     // here you enter the pin names for each of the functions, order matters!
-  Robot(int irLeft, int irRight, int motorLeftforward,
-  int motorLeftback, int motorRightforward, int motorRightback, 
-  int speed, int challenge):
-      irAnalogSensorLeft(irLeft),
-      irAnalogSensorRight(irRight),
-      motorLeftPinForward(motorLeftforward),
-      motorLeftPinBack(motorLeftback),
-      motorRightPinForward(motorRightforward),
-      motorRightPinBack(motorRightback),
-      speed(speed),
-      challengeNumber(challenge)
-    {}
-
+    Robot(int irLeft, int irRight, int motorLeftforward, int motorLeftback, int motorRightforward, int motorRightback, int speed, int challenge) :
+        irAnalogSensorLeft(irLeft),
+        irAnalogSensorRight(irRight),
+        motorLeftPinForward(motorLeftforward),
+        motorLeftPinBack(motorLeftback),
+        motorRightPinForward(motorRightforward),
+        motorRightPinBack(motorRightback),
+        speed(speed),
+        challengeNumber(challenge) {}
 
     void forward() {
       analogWrite(motorLeftPinForward, speed);
       analogWrite(motorLeftPinBack, 0);
       analogWrite(motorRightPinForward, speed);
       analogWrite(motorRightPinBack, 0);
-
       Serial.print("forward\n");
     }
 
@@ -77,8 +69,7 @@ class Robot {
       analogWrite(motorLeftPinBack, 0);
       analogWrite(motorRightPinForward, speed);
       analogWrite(motorRightPinBack, 0);
-      Serial.print("left\n");   
-
+      Serial.print("left\n");
     }
 
     void stop() {
@@ -89,21 +80,19 @@ class Robot {
       Serial.print("stop\n");
     }
 
-  // this function returns the values read by the IR sensor and may be useful to you
+    // this function returns the values read by the IR sensor and may be useful to you
     int getIntensity(int irAnalogPin) {
       int intensity = analogRead(irAnalogPin);
       Serial.print(intensity);
       return intensity;
     }
+};
 
-  };
+// what we did here is create a robot object that will be used throughout the program
+// you can experiment with speed, it just changes the amount of current used
+Robot robot(A0, A1, 5, 6, 9, 10, 1000, 1);  
 
-
-  // what we did here is create a robot object that will be used throughout the program
-  // you can experiment with speed, it just changes the amount of current used
-  Robot robot(A0, A1, 5,6,9,10, 1000, 1);  
-
-  void setup() {
+void setup() {
     Serial.begin(9600);
 
     // Set the IR sensor pins as input
@@ -118,59 +107,55 @@ class Robot {
 }
 
 const int IRtreshold = 500;
- 
+
 void loop() {
-  // these debugging statements may be useful to you
-  // view this output in serial monitor (control + shift + M)
-  Serial.print("IR left: ");
-  int IRleft = robot.getIntensity(robot.irAnalogSensorLeft);
-  Serial.print(" IR right: ");
-  int IRright = robot.getIntensity(robot.irAnalogSensorRight);
-  Serial.print("\n");
+    // these debugging statements may be useful to you
+    // view this output in serial monitor (control + shift + M)
+    Serial.print("IR left: ");
+    int IRleft = robot.getIntensity(robot.irAnalogSensorLeft);
+    Serial.print(" IR right: ");
+    int IRright = robot.getIntensity(robot.irAnalogSensorRight);
+    Serial.print("\n");
 
-// as you can see I accessed the challengeNumber property of the robot to find
-// which logic to run, you may find that the logic is the same for both challenges
-// // but hint: it is not 
-if (robot.challengeNumber == 1){
-    // // basic control flow to steer device (logic left as an exercise to teams)
-    if (IRleft < IRtreshold && IRright < IRtreshold ){
-      robot.forward();
+    // as you can see I accessed the challengeNumber property of the robot to find
+    // which logic to run, you may find that the logic is the same for both challenges
+    // // but hint: it is not 
+    if (robot.challengeNumber == 1) {
+        // // basic control flow to steer device (logic left as an exercise to teams)
+        if (IRleft < IRtreshold && IRright < IRtreshold) {
+            robot.forward();
+        }
+        else if (IRleft > IRtreshold && IRright < IRright) {
+            robot.turn_left();
+        }
+        else if (IRleft < IRtreshold && IRright > IRtreshold) {
+            robot.turn_right();
+        }
+        // else if (condition3) {
+        //   robot.reverse();
+        // }
+        else {
+            robot.stop();
+            delay(4000);
+            robot.forward();
+            delay(500);
+        }
     }
-    else if (IRleft>IRtreshold && IRright<IRright){
-      robot.turn_left();
-    }
-    else if (IRleft<IRtreshold&&IRright>IRtreshold){
-      robot.turn_right();
-    }
-    // else if (condition3){
-    //   robot.reverse();
+    // elif (robot.challengeNumber == 2) {
+    //     if (condition4) {
+    //       robot.forward();
+    //     }
+    //     elif (condition5) {
+    //       robot.turn_left();
+    //     }
+    //     elif (condition6) {
+    //       robot.turn_right();
+    //     }
+    //     elif (condition7) {
+    //       robot.reverse();
+    //     }
+    //     elif (condition8) {
+    //       robot.stop();
+    //     }
     // }
-    else {
-      robot.stop();
-      delay(4000);
-      robot.forward();
-      delay(500)
-    }
-    continue;
-  } 
-// elif (robot.challengeNumber == 2){
-//         if (condition4){
-//         robot.forward();
-//       }
-//       elif (condition5){
-//         robot.turn_left();
-//       }
-//       elif (condition6){
-//         robot.turn_right();
-//       }
-//       elif (condition7){
-//         robot.reverse();
-//       }
-//       elif (condition8) {
-//         robot.stop();
-//       }
-//   }
 }
-
-
-
